c44d4d5751addbee42b847f82c018117c5a89488
Iliyan Kulishev <iliyan89@users.noreply.github.com>
2019-11-28 13:23:31 +0100

    Add files via upload

---
+++ Models/ViewModels/SomeVM.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizNSwap.Models.ViewModels
+{
+    public class SomeVM
+    {
+    }
+}
