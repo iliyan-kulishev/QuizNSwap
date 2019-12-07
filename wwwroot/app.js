54a9161efde28f29be81799228a8df4af49bda91
Katarzyna Zielińska <eaakjz@students.eaaa.dk>
2019-12-07 15:29:40 +0100

    Style mobile sign up page

--- wwwroot/app.js
+++ wwwroot/app.js
@@ -228,7 +228,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65069" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55090" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
