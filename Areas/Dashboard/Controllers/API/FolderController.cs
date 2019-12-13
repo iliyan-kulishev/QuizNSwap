d4a2d264dc0fc5f739b7c53730dd17ce9c3ff49b
Iliyan Kulishev <iliyan89@users.noreply.github.com>
2019-12-13 04:38:43 +0100

    Done dashbord/folder rename

--- Areas/Dashboard/Controllers/API/FolderController.cs
+++ Areas/Dashboard/Controllers/API/FolderController.cs
@@ -9,6 +9,7 @@ using System.Security.Claims;
 using QuizNSwap.BusinessServices;
 using QuizNSwap.Data;
 using QuizNSwap.Data.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace QuizNSwap.Areas.Dashboard.Controllers.API
 {
@@ -22,6 +23,7 @@ namespace QuizNSwap.Areas.Dashboard.Controllers.API
         {
             this.dbContext = dbContext;
         }
+
 
         [HttpPost]
         public async Task<IActionResult> CreateFolder([FromBody] FolderDTO folder)
@@ -45,11 +47,10 @@ namespace QuizNSwap.Areas.Dashboard.Controllers.API
             else return BadRequest();
         }
 
+
         [HttpDelete("{id}")]
         public IActionResult DeleteFolder(int id)
         {
-            //long folderIdNum = Convert.ToInt64(id);
-
             var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
             var folderResult = dbContext.Folders.FirstOrDefault(f => f.Id == id && f.UserId == userId);
@@ -64,6 +65,46 @@ namespace QuizNSwap.Areas.Dashboard.Controllers.API
             dbContext.SaveChangesAsync();
 
             return Ok();
+        }
+
+
+
+        
+        [HttpPut]
+        public async Task<IActionResult> PutTodoItem([FromQuery(Name = "id")]long id, [FromQuery(Name = "folderName")]string folderName)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var folderIfExists = dbContext.Folders.FirstOrDefault(f => f.Id == id && f.UserId == userId);
+
+            /*
+            if (id != folder.Id)
+            {
+                return BadRequest();
+            }*/
+
+            folderIfExists.Name = folderName;
+
+
+            dbContext.Entry(folderIfExists).State = EntityState.Modified;
+
+            try
+            {
+                await dbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (folderIfExists==null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
         }
 
 
