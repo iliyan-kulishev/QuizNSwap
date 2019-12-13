0fb5a45090ad9cf0e868d05839efd66e5dd3b9be
Lukas <lukas@youtuberis.lt>
2019-12-13 10:05:11 +0100

    Add migration for gameplay section

--- Data/Models/Player.cs
+++ Data/Models/Player.cs
@@ -1,11 +1,15 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
 namespace QuizNSwap.Data.Models
 {
     public class Player
     {
-        public int Id {get; set;}
-        public string Name { get; set;}
-        public object Data { get; set; }
-        public string GameId { get; set; }
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
         public virtual Game Game { get; set; }
     }
 }
